00446a9c0fb7cf6fa7f151b9fde4d499ecc37bb4
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-10-27 18:41:43 +0100

    first exercice

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import "./App.css";
 
 
 class App extends Component {
+  constructor (){
+    super()
+    this.state={
+     
+      name:'taoufik'
+    }
+    console.log('constructor');
+  }
+
+  async componentDidMount(){
+    console.log('componentDidMount');
+   
+    this.setState({name:'toto'},()=>{console.log(this.state.name)})
+    
+  }
+
+
   render() {
-    return <div className="App"></div>;
+    console.log('render');
+  
+    return (
+        <div className="App">
+          <h1>{this.state.name}</h1>
+        </div>
+    )
   }
 }
 
