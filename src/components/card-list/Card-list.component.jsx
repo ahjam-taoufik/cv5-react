232e6d86e7e0252657f8c9990ddaed7e4fe205a0
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-10-27 23:21:27 +0100

    add image in card-list

--- src/components/card-list/Card-list.component.jsx
+++ src/components/card-list/Card-list.component.jsx
@@ -1,14 +1,15 @@
 import { Component } from 'react'
-
+import './card-list.styles.css'
 export default class CardList extends Component {
   render() {
     const {usersFiltred}=this.props
     // console.log(usersFiltred);
     return (
-      <div>
+      <div className='card-list'>
           { usersFiltred.map(user=>{
             return(
-              <div key={user.id}>
+              <div className='card-container' key={user.id}>
+                <img src={`https://robohash.org/${user.id}?size=200x200`} />
                <h1>{user.name}</h1> 
               </div>
             )
