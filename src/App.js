60f360df55ae09c10b0db1d5761f808d078af604
Ahjam Taoufik <taoufikahjam@gmail.com>
2022-10-28 18:17:36 +0100

    use useEffect in filtred

--- src/App.js
+++ src/App.js
@@ -3,44 +3,48 @@ import "./App.css";
 import CardList from "./components/card-list/Card-list.component";
 import SearchBox from "./components/search-box/Search-box.component";
 
+const App = () => {
+  console.log("render . . .");
+  const [searchfield, setSearchfield] = useState("");
+  const [users, setUsers] = useState([]);
+  const [usersFiltred, setusersFiltred] = useState(users);
 
-const App=()=> {
-  const [searchfield, setSearchfield] = useState('')
-  const [users, setUsers] = useState([])
- 
-useEffect(() => {
-    const fetchUser=async()=>{
-      const res = await fetch('https://jsonplaceholder.typicode.com/users')
-      const data=await res.json()
-      setUsers(data)
-    }
-
-    fetchUser()
-}, [])
-
-
-   const handlerChange=(event)=>{
-    const searchfieldLowerCase=event.target.value.toLowerCase()
-    setSearchfield(searchfieldLowerCase)
-    
-   }
-   
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setUsers(data);
+    };
+    fetchUser();
+  }, []);
   
 
+useEffect(() => {
+ 
+  const usersFiltredHandler = users.filter((user) => {
+    return user.name.toLowerCase().includes(searchfield);
+  });
+  setusersFiltred(usersFiltredHandler)
 
-  const usersFiltred=users.filter((user)=>{
-   return user.name.toLowerCase().includes(searchfield)
-   })
+}, [searchfield, users])
 
 
-    return (
-        <div className="App">
-          <SearchBox
-           placeholder='enter user'
-           onhandlerChange={handlerChange} 
-           />
-           <CardList usersFiltred={usersFiltred} />
-        </div>
-    )
-  }
+
+
+  const handlerChange = (event) => {
+    const searchfieldLowerCase = event.target.value.toLowerCase();
+    setSearchfield(searchfieldLowerCase);
+  };
+
+
+
+  return (
+    <div className="App">
+      <SearchBox placeholder="enter user" onhandlerChange={handlerChange} />
+     
+
+      <CardList usersFiltred={usersFiltred} />
+    </div>
+  );
+};
 export default App;
